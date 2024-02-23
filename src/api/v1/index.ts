1cc0440d939af2e39dac131b8c0c5354ffcc0fed
DJ13423 <djmonarch50@gmail.com>
2024-02-22 17:23:08 -0800

    endpont aliases

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -6,5 +6,11 @@ router.use('/broadcast', require('./broadcast').default)
 router.use('/database', require('./database').default)
 router.use('/no-delete-database', require('./no-delete-database').default)
 
+// aliases
+router.use('/db', require('./database').default)
+router.use('/nddb', require('./no-delete-database').default)
+router.use('/no-delete-db', require('./no-delete-database').default)
+router.use('/no-del-db', require('./no-delete-database').default)
+
 
 export default router
